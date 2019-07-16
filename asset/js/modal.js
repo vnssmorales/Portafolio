18b423dbd5c348a8a0eb834efcffa6a8a2aad611
vnssmorales <vnss.morales@gmail.com>
2019-07-15 22:44:24 -0400

    agrego habilidades

--- asset/js/modal.js
+++ asset/js/modal.js
@@ -1,16 +1,23 @@
+
+
 $(function(){
     var control  = 0;
     var index    = 0;
     var $galeria;
+    var total    = 0;
 
     $("#proyectos-container a").on("click", function(){
         index = $(this).index();
         $("#modal").fadeIn();
-        $("#modal .coleccion-modal").hide();
-        $("#modal .coleccion-modal").eq(index).fadeIn();
+        $("#modal .modal-proyectos").hide();
+        $("#modal .modal-proyectos").eq(index).fadeIn();
 
-        $("#modal .coleccion-modal").eq(index).children().eq(0).children().hide();
-        $("#modal .coleccion-modal").eq(index).children().eq(0).children().eq(0).fadeIn();
+        $("#modal .modal-proyectos").eq(index).children().eq(0).children().hide();
+        $("#modal .modal-proyectos").eq(index).children().eq(0).children().eq(0).fadeIn(); 
+        
+        control = 0;
+        $galeria = $("#" + $(this).attr("href"));
+        total = $galeria.children().size();
         return false;
     });
 })
